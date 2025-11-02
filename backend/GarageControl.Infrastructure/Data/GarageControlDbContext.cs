b9b798643bd3c11de232fa42aafbf638787dd84c
AlexanederNedelkin <avnedelkin@gmail.com>
2025-11-02 18:09:05 +0200

    Initial migration

---
+++ backend/GarageControl.Infrastructure/Data/GarageControlDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using GarageControl.Infrastructure.Data.Models;
+
+namespace GarageControl.Infrastructure.Data
+{
+    public class GarageControlDbContext : IdentityDbContext<User>
+    {
+        public GarageControlDbContext(DbContextOptions<GarageControlDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
