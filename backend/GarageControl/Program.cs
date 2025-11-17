98adb258fe9af10624f6ce43a83ed3332ce2eacf
AlexanederNedelkin <avnedelkin@gmail.com>
2025-11-17 11:58:39 +0200

    Added Authentication stuff

--- backend/GarageControl/Program.cs
+++ backend/GarageControl/Program.cs
@@ -1,7 +1,11 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 using GarageControl.Infrastructure.Data;
+using GarageControl.Infrastructure.Data.Models;
 using GarageControl.Infrastructure.Common;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,46 @@ builder.Services.AddDbContext<GarageControlDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IRepository, Repository>();
+
+builder.Services.AddIdentity<User, IdentityRole>(o => o.SignIn.RequireConfirmedAccount = false)
+               .AddEntityFrameworkStores<GarageControlDbContext>()
+               .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey("ab78f6c7-dda7-4898-92b9-1000b2a3894f"u8.ToArray()),
+        ValidIssuer = "https://localhost:5058",
+        ValidAudience = "https://localhost:5058",
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.Strict;
+});
+
+builder.Services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 5058;
+            });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
